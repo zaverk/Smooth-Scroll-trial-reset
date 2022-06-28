5b7c7b241daab37927d28282022827bbcf960120
zaverk <dot@DESKTOP-B8J6DDE>
2022-06-28 17:36:07 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace stss
+{
+    public static class Ssts
+    {
+
+        public static void Main()
+        {
+            foreach (var pr in Process.GetProcessesByName("SmoothScroll"))
+            {
+                pr.Kill();
+            }
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            int secondsSinceEpoch = (int)t.TotalSeconds;
+            int dot = 1;
+            string user_value = "1";
+            try
+            {
+                dot = Registry.CurrentUser.OpenSubKey(@"Software\SmoothScroll").GetValue("kSSClientId").ToString().IndexOf('.');
+                user_value = Registry.CurrentUser.OpenSubKey(@"Software\SmoothScroll").GetValue("kSSClientId").ToString()[..dot];
+            }
+            catch
+            {
+                Registry.CurrentUser.CreateSubKey(@"Software\SmoothScroll").SetValue("kSSClientId", user_value + '.' + secondsSinceEpoch);
+            }
+            Registry.CurrentUser.CreateSubKey(@"Software\SmoothScroll").SetValue("kSSClientId", user_value + '.' + secondsSinceEpoch);
+            Registry.CurrentUser.CreateSubKey(@"Software\SmoothScroll").SetValue("kSSInstallDate", secondsSinceEpoch, RegistryValueKind.String);
+            Process.Start("SmoothScroll.exe");
+        }
+    }
+}
